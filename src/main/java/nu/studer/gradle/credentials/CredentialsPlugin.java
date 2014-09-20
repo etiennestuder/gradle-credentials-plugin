dd218c4ad03379c34ef69c84a852da3689d640a7
etiennestuder <etienne@studer.nu>
2014-09-20 23:48:04 +0200

    Initial (empty) plugin to satisfy the build

---
+++ src/main/java/nu/studer/gradle/credentials/CredentialsPlugin.java
@@ -0,0 +1,16 @@
+package nu.studer.gradle.credentials;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * Plugin to store and access encrypted credentials.
+ */
+public class CredentialsPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+
+    }
+
+}
