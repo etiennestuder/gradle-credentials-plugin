620d6f8ceed768a03c5604a5bb60439f90b54790
etiennestuder <etienne@studer.nu>
2014-09-25 23:52:35 +0200

    Add MD5 class and test.

---
+++ src/main/groovy/nu/studer/gradle/util/MD5.java
@@ -0,0 +1,30 @@
+package nu.studer.gradle.util;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+
+/**
+ * Utilities related to MD5 hashing.
+ */
+public final class MD5 {
+
+    private MD5() {
+    }
+
+    /**
+     * Calculates the MD5 hash for the given string and returns it in HEX format.
+     *
+     * @param string the string to hash
+     * @return the resulting MD5 hash as a string in HEX format
+     */
+    public static String generateMD5Hash(String string) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            byte[] hashedBytes = digest.digest(string.getBytes("UTF-8"));
+            return DatatypeConverter.printHexBinary(hashedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot generate MD5 hash for string '" + string + "': " + e.getMessage(), e);
+        }
+    }
+
+}
---
+++ src/test/groovy/nu/studer/gradle/util/MD5Test.java
@@ -0,0 +1,31 @@
+package nu.studer.gradle.util;
+
+import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MD5Test {
+
+    @Test
+    public void generateMD5Hash() throws Exception {
+        String input = "some string";
+
+        String result = MD5.generateMD5Hash(input);
+
+        for (char c : result.toCharArray()) {
+            boolean isHex = '0' <= c && c <= '9' || 'A' <= c && c <= 'F';
+            assertTrue("Character must be HEX: " + c, isHex);
+        }
+    }
+
+    @Test
+    public void convertByteArrayToHexString() throws Exception {
+        byte[] bytes = new byte[]{-128, -1, 0, 9, 10, 11, 17, 127};
+        String hex = DatatypeConverter.printHexBinary(bytes);
+        assertEquals("80FF00090A0B117F", hex);
+    }
+
+}
