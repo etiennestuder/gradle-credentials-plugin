c40f308ef303c5ca89995f99c8131d775fd88d3a
etiennestuder <etienne@studer.nu>
2014-09-26 07:46:15 +0200

    Write passphrase all lower-case.

--- src/test/groovy/nu/studer/gradle/util/EncryptionTest.java
+++ src/test/groovy/nu/studer/gradle/util/EncryptionTest.java
@@ -8,10 +8,10 @@ public class EncryptionTest {
 
     @Test
     public void encryptDecrypt() {
-        char[] passPhrase = "My very secret pass phrase".toCharArray();
+        char[] passphrase = "My very secret pass phrase".toCharArray();
         String textToEncrypt = "Some text that needs to be encrypted.";
 
-        Encryption encryption = Encryption.createEncryption(passPhrase);
+        Encryption encryption = Encryption.createEncryption(passphrase);
         String encrypted = encryption.encrypt(textToEncrypt);
         String decrypted = encryption.decrypt(encrypted);
 
