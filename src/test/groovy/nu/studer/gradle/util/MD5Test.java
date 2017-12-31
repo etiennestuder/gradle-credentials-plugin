0cf4658ca738c91bcdb4e1b670262302e5e6f4e7
etiennestuder <etienne@studer.nu>
2017-12-31 10:02:07 +0100

    Remove redundant test

--- src/test/groovy/nu/studer/gradle/util/MD5Test.java
+++ src/test/groovy/nu/studer/gradle/util/MD5Test.java
@@ -2,7 +2,6 @@ package nu.studer.gradle.util;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MD5Test {
@@ -17,13 +16,6 @@ public class MD5Test {
             boolean isHex = '0' <= c && c <= '9' || 'A' <= c && c <= 'F';
             assertTrue("Character must be HEX: " + c, isHex);
         }
-    }
-
-    @Test
-    public void convertByteArrayToHexString() {
-        byte[] bytes = new byte[]{-128, -1, 0, 9, 10, 11, 17, 127};
-        String hex = Base64.printHexBinary(bytes);
-        assertEquals("80FF00090A0B117F", hex);
     }
 
 }
