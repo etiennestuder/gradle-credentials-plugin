9805a3370e6304456460c8411db9f34ae9b78ffa
etiennestuder <etienne@studer.nu>
2017-12-31 10:36:19 +0100

    Ensure encoding/decoding is compatible with current plugin versions

--- src/main/groovy/nu/studer/gradle/util/Base64.java
+++ src/main/groovy/nu/studer/gradle/util/Base64.java
@@ -17,8 +17,7 @@ public final class Base64 {
      * @return the resulting Base64 string
      */
     public static String encodeBase64(byte[] bytes) {
-        return org.apache.commons.codec.binary.Base64.encodeBase64String(bytes);
-//        return StringUtils.newStringUtf8(org.apache.commons.codec.binary.Base64.encodeBase64(bytes, false));
+        return base64Instance().encodeToString(bytes);
     }
 
     /**
@@ -28,7 +27,7 @@ public final class Base64 {
      * @return the resulting bytes
      */
     public static byte[] decodeBase64(String string) {
-        return org.apache.commons.codec.binary.Base64.decodeBase64(string);
+        return base64Instance().decode(string);
     }
 
     /**
@@ -39,6 +38,10 @@ public final class Base64 {
      */
     public static String printHexBinary(byte[] bytes) {
         return new String(encodeHex(bytes, false));
+    }
+
+    private static org.apache.commons.codec.binary.Base64 base64Instance() {
+        return new org.apache.commons.codec.binary.Base64(0, new byte[]{'\r', '\n'}, false);
     }
 
 }
---
+++ src/test/groovy/nu/studer/gradle/util/Base64Java9CompatibilityTest.java
@@ -0,0 +1,32 @@
+package nu.studer.gradle.util;
+
+import groovy.json.internal.Charsets;
+import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public final class Base64Java9CompatibilityTest {
+
+    @Test
+    public void librariesAreCompatibleInEncoding() {
+        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        assertEquals(Base64.encodeBase64(bytes), DatatypeConverter.printBase64Binary(bytes));
+    }
+
+    @Test
+    public void librariesAreCompatibleInDecoding() {
+        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        String encoded = DatatypeConverter.printBase64Binary(bytes);
+        assertArrayEquals(Base64.decodeBase64(encoded), DatatypeConverter.parseBase64Binary(encoded));
+    }
+
+    @Test
+    public void librariesAreCompatibleInPrinting() {
+        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        assertEquals(Base64.printHexBinary(bytes), DatatypeConverter.printHexBinary(bytes));
+    }
+
+}
