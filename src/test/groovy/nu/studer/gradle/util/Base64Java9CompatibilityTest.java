7b096d8cb294e0b36f53a00f2bb69faaaf4a9977
Simon Nagl <simon.nagl@e-mundo.de>
2018-11-29 13:53:34 +0100

    Use java API for Charset UTF-8

--- src/test/groovy/nu/studer/gradle/util/Base64Java9CompatibilityTest.java
+++ src/test/groovy/nu/studer/gradle/util/Base64Java9CompatibilityTest.java
@@ -1,7 +1,7 @@
 package nu.studer.gradle.util;
 
-import org.gradle.internal.impldep.com.google.common.base.Charsets;
 import org.junit.Test;
+import java.nio.charset.StandardCharsets;
 
 import javax.xml.bind.DatatypeConverter;
 
@@ -12,20 +12,20 @@ public final class Base64Java9CompatibilityTest {
 
     @Test
     public void librariesAreCompatibleInEncoding() {
-        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        byte[] bytes = "some text".getBytes(StandardCharsets.UTF_8);
         assertEquals(Base64.encodeBase64(bytes), DatatypeConverter.printBase64Binary(bytes));
     }
 
     @Test
     public void librariesAreCompatibleInDecoding() {
-        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        byte[] bytes = "some text".getBytes(StandardCharsets.UTF_8);
         String encoded = DatatypeConverter.printBase64Binary(bytes);
         assertArrayEquals(Base64.decodeBase64(encoded), DatatypeConverter.parseBase64Binary(encoded));
     }
 
     @Test
     public void librariesAreCompatibleInPrinting() {
-        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        byte[] bytes = "some text".getBytes(StandardCharsets.UTF_8);
         assertEquals(Base64.printHexBinary(bytes), DatatypeConverter.printHexBinary(bytes));
     }
 
