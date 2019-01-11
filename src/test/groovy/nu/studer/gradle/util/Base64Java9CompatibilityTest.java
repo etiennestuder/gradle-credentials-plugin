a327a12ebaa586f2447c5701a4cb85292e1905f1
Etienne Studer <etienne@studer.nu>
2019-01-10 20:49:19 -0800

    Make Java 8 compatible

--- src/test/groovy/nu/studer/gradle/util/Base64Java9CompatibilityTest.java
+++ src/test/groovy/nu/studer/gradle/util/Base64Java9CompatibilityTest.java
@@ -1,9 +1,9 @@
 package nu.studer.gradle.util;
 
-import groovy.json.internal.Charsets;
 import org.junit.Test;
 
 import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -12,20 +12,20 @@ public final class Base64Java9CompatibilityTest {
 
     @Test
     public void librariesAreCompatibleInEncoding() {
-        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        byte[] bytes = "some text".getBytes(StandardCharsets.UTF_8);
         assertEquals(Base64.encodeBase64(bytes), DatatypeConverter.printBase64Binary(bytes));
     }
 
     @Test
     public void librariesAreCompatibleInDecoding() {
-        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        byte[] bytes = "some text".getBytes(StandardCharsets.UTF_8);
         String encoded = DatatypeConverter.printBase64Binary(bytes);
         assertArrayEquals(Base64.decodeBase64(encoded), DatatypeConverter.parseBase64Binary(encoded));
     }
 
     @Test
     public void librariesAreCompatibleInPrinting() {
-        byte[] bytes = "some text".getBytes(Charsets.UTF_8);
+        byte[] bytes = "some text".getBytes(StandardCharsets.UTF_8);
         assertEquals(Base64.printHexBinary(bytes), DatatypeConverter.printHexBinary(bytes));
     }
 
