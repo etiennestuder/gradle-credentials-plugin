fd9228291c10082e2d0dd8e1c517b8b571a28151
Etienne Studer <etienne@studer.nu>
2019-01-11 18:08:38 -0800

    Use @Internal

--- src/main/groovy/nu/studer/gradle/credentials/AddCredentialsTask.java
+++ src/main/groovy/nu/studer/gradle/credentials/AddCredentialsTask.java
@@ -5,6 +5,7 @@ import nu.studer.gradle.credentials.domain.CredentialsPersistenceManager;
 import nu.studer.java.util.OrderedProperties;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.internal.tasks.options.Option;
+import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.slf4j.Logger;
@@ -45,12 +46,12 @@ public class AddCredentialsTask extends DefaultTask {
         this.value = value;
     }
 
-    // do not annotate as @Input to avoid the key being stored in the task artifact cache
+    @Internal("Do not annotate as @Input to avoid the key being stored in the task artifact cache")
     public String getCredentialsKey() {
         return key != null ? key : getProjectProperty(CredentialsPlugin.CREDENTIALS_KEY_PROPERTY);
     }
 
-    // do not annotate as @Input to avoid the value being stored in the task artifact cache
+    @Internal("Do not annotate as @Input to avoid the value being stored in the task artifact cache")
     public String getCredentialsValue() {
         return value != null ? value : getProjectProperty(CredentialsPlugin.CREDENTIALS_VALUE_PROPERTY);
     }
